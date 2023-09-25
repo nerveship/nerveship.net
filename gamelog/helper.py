f4f4262dc60d34a2c40f9dec31204bb4c5133f93
nerveship <johnstuartgonsales@gmail.com>
2023-09-25 03:59:49 +0100

    added comments

--- gamelog/helper.py
+++ gamelog/helper.py
@@ -1,12 +1,15 @@
+#Init vars
 categories = ["Title", "Developer", "Publisher", "Genre", "Platform", "Release", "Completion", "Rating"]
 answers = []
 
+#Gets input for each category and adds it to answers
 def get_input(array):
     for i in array:
         title = i + ": "
         answer = input(title)
         answers.append(answer)
 
+#Generates the correctly formatted html
 def print_html():
     print("<tr>")
     for i in answers:
@@ -14,5 +17,4 @@ def print_html():
     print("</tr>")
 
 get_input(categories)
-
 print_html()
