e694252085ba6b38de3377b857563aeeb4099381
nerveship <johnstuartgonsales@gmail.com>
2023-09-29 06:00:47 +0100

    fixed cpp version

--- gamelog/helper.cpp
+++ gamelog/helper.cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 int main()
 {
-    string Categories[8] = {"Title", "Developer", "Publisher", "Genre", "Platform", "Completion", "Rating"};
+    string Categories[8] = {"Title", "Developer", "Publisher", "Genre", "Platform", "Release", "Completion", "Rating"};
     string Answers[8];
 
-    for (int i = 0; i < 7; i++)
+    //Get answers for each category
+    for (int i = 0; i < 8; i++)
     {
         cout << Categories[i] << ": ";
-        cin >> Answers[i];
+        getline(cin, Answers[i]);
     }
 
+    //Print out the formatted html
     cout << "<tr>\n";
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 8; i++)
     {
-        cout << "    <td class=\"fitwidth\">" + Answers[i] + "</td>\n";
+        cout << "    <td class=\"fitwidth\">" + Answers[i] + "</td>\n"; 
     }
     cout << "</tr>\n";
 }
