93c30064775c5b7c97c013d73f84205c127147d3
nerveship <johnstuartgonsales@gmail.com>
2024-07-16 20:51:14 +0100

    added cpp stuff

---
+++ gamelog/gamelogcpp/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string Categories[8] = { "Title", "Developer", "Publisher", "Genre", "Platform", "Release", "Completion", "Rating" };
+    std::string Answers[8];
+
+    //Get answers for each category
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << Categories[i] << ": ";
+        std::getline(std::cin, Answers[i]);
+    }
+
+    //Clear terminal
+    std::cout << "\033[2J\033[1;1H";
+
+    //Print out the formatted html
+    std::cout << "<tr>\n";
+    for (int i = 0; i < 8; i++)
+    {
+        std::cout << "    <td class=\"fitwidth\">" + Answers[i] + "</td>\n";
+    }
+    std::cout << "</tr>\n";
+}
