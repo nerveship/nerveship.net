b555a0c78b7fbec21b12763e3af693521b769da4
nerveship <johnstuartgonsales@gmail.com>
2024-10-11 23:18:26 +0100

    added another case to tweet format

--- artlog/helper.py
+++ artlog/helper.py
@@ -24,6 +24,8 @@ def format_tweet():
     print("-------------")
     if answers[0].lower() == "anime":
         print("Artlog add! " + answers[1] + " - An " + answers[0].lower()  + " made by " + answers[2] + ", released in " + answers[3][-4:] + "." + "\nRated: " + answers[5] + ".")
+    elif answers[0].lower() == "book" or "short story":
+        print("Artlog add! " + answers[1] + " - A " + answers[0].lower()  + " by " + answers[2] + ", released in " + answers[3][-4:] + "." + "\nRated: " + answers[5] + ".")
     else:
         print("Artlog add! " + answers[1] + " - A " + answers[0].lower()  + " made by " + answers[2] + ", released in " + answers[3][-4:] + "." + "\nRated: " + answers[5] + ".")
     print("https://www.nerveship.net/artlog/")
