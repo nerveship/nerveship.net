33fea9c3d5a73103e1654fd584776c5419ad6921
nerveship <johnstuartgonsales@gmail.com>
2024-12-01 17:59:04 +0000

    added ini

--- artlog/helper.py
+++ artlog/helper.py
@@ -1,35 +1,53 @@
-import os
+# Initialize variables
+import logging
 
-#init vars
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 categories = ["Type", "Title", "Creator", "Release", "Completion", "Rating"] 
-answers = []
-final_string = ''
 
-#gets input for each category and adds it to answers
-def get_input(array):
-    for i in array:
-        title = i + ": "
-        answer = input(title)
-        answers.append(answer)
+# Collects input for each category and adds it to a list
+def collect_user_input(categories):
+    answers = []
+    for category in categories:
+        title = category + ": "
+        while True:
+            answer = input(title)
+            if answer.strip():
+                answers.append(answer)
+                break
+            else:
+                print("Input cannot be empty. Please provide a valid response.")
+    return answers
 
-#generates the correctly formatted html
-def format_html():
-    final_string = "<tr>\n"
-    for i in answers:
-        final_string += "    <td>" + i + "</td>\n"
-    final_string += "</tr>"
-    print(final_string)
+# Generates the correctly formatted HTML
+def format_html(answers):
+    if len(answers) != len(categories):
+        raise ValueError("The number of answers provided does not match the expected number of categories.")
+    
+    html_string = "<tr>\n"
+    for answer in answers:
+        html_string += "    <td>" + answer + "</td>\n"
+    html_string += "</tr>"
+    return html_string
 
-def format_tweet():
-    print("-------------")
-    if answers[0].lower() == "anime":
-        print("Artlog add! " + answers[1] + " - An " + answers[0].lower()  + " made by " + answers[2] + ", released in " + answers[3][-4:] + "." + "\nRated: " + answers[5] + ".")
-    elif answers[0].lower() == "book" or "short story":
-        print("Artlog add! " + answers[1] + " - A " + answers[0].lower()  + " by " + answers[2] + ", released in " + answers[3][-4:] + "." + "\nRated: " + answers[5] + ".")
-    else:
-        print("Artlog add! " + answers[1] + " - A " + answers[0].lower()  + " made by " + answers[2] + ", released in " + answers[3][-4:] + "." + "\nRated: " + answers[5] + ".")
-    print("https://www.nerveship.net/artlog/")
+# Generates the formatted tweet
+def format_tweet(answers):
+    article = "An" if answers[0].lower() in ["anime", "album"] else "A"
+    release_year = "unknown"
+    if len(answers[3]) >= 4 and answers[3][-4:].isdigit():
+        release_year = answers[3][-4:]
+    
+    tweet = (f"-------------\n"
+             f"Artlog add! {answers[1]} - {article} {answers[0].lower()} by {answers[2]}, "
+             f"released in {release_year}.\nRated: {answers[5]}.\n"
+             f"https://www.nerveship.net/artlog/")
+    return tweet
 
-get_input(categories)
-format_html()
-format_tweet()
+# Main flow
+answers = collect_user_input(categories)
+html_output = format_html(answers)
+tweet_output = format_tweet(answers)
+
+print(html_output)
+print(tweet_output)
