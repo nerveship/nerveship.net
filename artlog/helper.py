a752068abe376af5e0bd14db99af6fe5b3af69d0
nerveship <johnstuartgonsales@gmail.com>
2025-01-11 09:55:40 +0000

    uncapitalised comments <- UGLY

--- artlog/helper.py
+++ artlog/helper.py
@@ -1,7 +1,7 @@
-# Define the categories of information to collect from the user
+#define the categories of information to collect from the user
 categories = ["Type", "Title", "Creator", "Release", "Completion", "Recommended"] 
 
-# Collects input for each category and adds it to a list
+#collects input for each category and adds it to a list
 def collect_user_input(categories):
     """
     Prompts the user to input information for each category.
@@ -12,18 +12,18 @@ def collect_user_input(categories):
         title = category + ": "
         while True:
             answer = input(title)
-            if answer.strip():  # Ensure the input is not empty or just whitespace
+            if answer.strip():  #ensure the input is not empty or just whitespace
                 answers.append(answer)
                 break
             else:
                 print("Input cannot be empty. Please provide a valid response.")
     return answers
 
-# Generates the correctly formatted HTML
+#generates the correctly formatted HTML
 def format_html(answers):
     html_string = "<tr>\n"
     for i, answer in enumerate(answers):
-        if i == len(answers) - 1:  # Check if it's the last iteration
+        if i == len(answers) - 1:  #check if it's the last iteration
             html_string += '    <td class="alnright">' + answer + "</td>\n"
         else:
             html_string += "    <td>" + answer + "</td>\n"
@@ -31,11 +31,11 @@ def format_html(answers):
     return html_string
 
 
-# Generates the formatted tweet
+#generates the formatted tweet
 def format_tweet(answers):
     article = "An" if answers[0].lower() in ["anime"] else "A"
     release_year = "unknown"
-    # Extract the last 4 characters of the release information if they represent a year
+    #extract the last 4 characters of the release information if they represent a year
     if len(answers[3]) >= 4 and answers[3][-4:].isdigit():
         release_year = answers[3][-4:]
     
@@ -45,11 +45,11 @@ def format_tweet(answers):
              f"https://www.nerveship.net/artlog/")
     return tweet
 
-# Main flow of the script
-answers = collect_user_input(categories)  # Collects user input for each category
-html_output = format_html(answers)  # Formats the input into HTML
-tweet_output = format_tweet(answers)  # Formats the input into a tweet
+#main flow of the script
+answers = collect_user_input(categories)  #collects user input for each category
+html_output = format_html(answers)  #formats the input into HTML
+tweet_output = format_tweet(answers)  #formats the input into a tweet
 
-# Print the outputs
+#print the outputs
 print(html_output)
 print(tweet_output)
