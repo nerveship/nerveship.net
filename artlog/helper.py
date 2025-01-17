8e8acb311f06f3994646a97f6ec3f20db549b534
nerveship <johnstuartgonsales@gmail.com>
2025-01-17 20:33:38 +0000

    added losp

--- artlog/helper.py
+++ artlog/helper.py
@@ -1,65 +1,31 @@
 import os
 
-categories = ["Type", "Title", "Creator", "Release", "Completion", "Recommended"]
+#init vars
+categories = ["Type", "Title", "Creator", "Release", "Completion", "Recommendation"] 
+answers = []
+final_string = ''
 
-def collect_user_input(categories):
-    answers = []
+#gets input for each category and adds it to answers
+def get_input(array):
+    for i in array:
+        title = i + ": "
+        answer = input(title)
+        answers.append(answer)
 
-    for category in categories:
-        title = category + ": "
+#generates the correctly formatted html
+def format_html():
+    final_string = "<tr>\n"
+    for i in answers:
+        final_string += "    <td>" + i + "</td>\n"
+    final_string += "</tr>"
+    print(final_string)
 
-        while True:
-            answer = input(title)
-            if answer.strip():
-                answers.append(answer)
-                break
-            else:
-                print("Input cannot be empty. Please provide a valid response.")
+def format_tweet():
+    print("-------------")
+    article = "An" if answers[0].lower() in ['anime', 'album'] else "A"
+    print("Artlog add! " + answers[1] + " - " + article + " " + answers[0].lower()  + " made by " + answers[2] + ", released in " + answers[3][-4:] + "." + "\nDo I Recommend it? " + answers[5] + ".")
+    print("https://www.nerveship.net/artlog/")
 
-    return answers
-
-def format_html(answers):
-    html_string = "<tr>\n"
-
-    for i, answer in enumerate(answers):
-        if i == len(answers) - 1:
-            html_string += '    <td class="alnright">' + answer + "</td>\n"
-        else:
-            html_string += "    <td>" + answer + "</td>\n"
-
-    html_string += "</tr>\n"
-
-    return html_string
-
-def insert_at_line(html_string, file_name="artlog/index.html", line_number=42):
-    try:
-        if not os.path.exists(file_name):
-            print(f"The file '{file_name}' does not exist.")
-            return
-        
-        with open(file_name, "r", encoding="utf-8") as file:
-            lines = file.readlines()
-        
-        while len(lines) < line_number:
-            lines.append("\n")
-        
-        indented_html = "\n".join(["\t" * 9 + line for line in html_string.split("\n") if line.strip() != ""])
-        
-        lines.insert(line_number - 1, "\n")
-        lines.insert(line_number, indented_html + "\n")
-        
-        with open(file_name, "w", encoding="utf-8") as file:
-            file.writelines(lines)
-        
-        print(f"HTML entry successfully added at line {line_number} in '{file_name}'")
-
-    except Exception as e:
-        print(f"An error occurred while modifying the file: {e}")
-
-answers = collect_user_input(categories)
-html_output = format_html(answers)
-
-print("Generated HTML:")
-print(html_output)
-
-insert_at_line(html_output, file_name="artlog/index.html", line_number=42)
+get_input(categories)
+format_html()
+format_tweet()  
